9eff74d340536e10949ab6ed520da8d785e64722
Stefan Hagen <stefan@hagen.link>
2021-09-14 13:00:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <array>
+#include <string>
+#include <sstream>
+
+#include <doctest/doctest.h>
+
 #include <fmt/format.h>
 
-int main() {
-  fmt::print("Hello, {}!\n", 42);
+
+TEST_SUITE ("Example derived tests.") {
+    TEST_CASE ("Hello 42.") {
+        /* Example snippet:
+         *
+         * fmt::print("Hello, {}!\n", 42);
+         */
+        auto const payload = "Hello 42.";
+        auto const sut = fmt::format("Hello {}.", 42);
+        REQUIRE(sut == payload);
+    }
 }
